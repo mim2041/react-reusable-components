9f98c1d151e40c71195c6271d5ec8d05ebcf9a03
mim2041 <suraiyamim338@gmail.com>
2024-05-16 11:12:52 +0600

    container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import MainLayout from "./components/Layout/MainLayout"
+import Container from "./components/ui/Container"
 
 
 function App() {
 
   return (
-    <div>
-        <MainLayout />
-    </div>
+    <Container>
+      <div className="h-screen w-full flex justify-center items-center">
+        <button className="px-3 py-2 bg-purple-400 rounded-lg"> This is a button</button>
+      </div>
+    </Container>
   )
 }
 
